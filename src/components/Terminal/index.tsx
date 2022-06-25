b9c844d20c7adc132a895ab8c6ae421ca28fad04
Andres Florez Lopez <aandresflorezlopez@gmail.com>
2022-06-24 20:24:35 -0500

    refactor THeader and spaces in mobile

--- src/components/Terminal/index.tsx
+++ src/components/Terminal/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Typewriter from 'typewriter-effect';
 
 import clsx from 'clsx';
@@ -8,7 +8,55 @@ const description = ['Hello guys...'];
 // Menlo-Regular, Menlo, monospace
 // `Hello guys... \n -- I'm Andres Florez as engineer that love beer, coding, travel and dream to become a DJ's some day`,
 
+const TerminalHeader = () => {
+  return (
+    <div id="terminal-header" className="flex flex-row">
+      <div
+        id="terminal-buttons"
+        className={clsx('flex flex-row w-4/12 justify-around p-4', 'md:w-2/12')}
+      >
+        <div
+          className={clsx(
+            'w-4 h-4 text-primary-50 justify-center',
+            'lg:w-6 lg:h-6 bg-redMac rounded-full flex'
+          )}
+        />
+        <div
+          className={clsx(
+            'w-4 h-4',
+            'lg:w-6 lg:h-6 bg-yellowMac rounded-full flex'
+          )}
+        />
+        <div
+          className={clsx(
+            'w-4 h-4',
+            'lg:w-6 lg:h-6 bg-greenMac rounded-full flex'
+          )}
+        />
+      </div>
+      <div id="terminal-tabs" className="flex w-11/12">
+        <div
+          className={clsx(
+            'flex w-4/12 justify-center items-center pt-4 pb-4 text-primary-50 font-menlo border-b-2 border-solid',
+            'md:w-3/12'
+          )}
+        >
+          about-me
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const Terminal = () => {
+  const body = useRef<HTMLDivElement>(null);
+
+  const makeScrollDown = () => {
+    body.current?.scrollTo({
+      top: body.current?.scrollHeight - body.current?.clientHeight,
+    });
+  };
+
   return (
     <div
       id="terminal"
@@ -19,40 +67,16 @@ const Terminal = () => {
       )}
     >
       {/* terminal header */}
-      <div id="terminal-header" className="flex flex-row">
-        <div
-          id="terminal-buttons"
-          className={clsx('flex flex-row w-2/12 justify-around p-4')}
-        >
-          <div
-            className={clsx(
-              'w-4 h-4 text-primary-50 justify-center',
-              'lg:w-6 lg:h-6 bg-redMac rounded-full flex'
-            )}
-          />
-          <div
-            className={clsx(
-              'w-4 h-4',
-              'lg:w-6 lg:h-6 bg-yellowMac rounded-full flex'
-            )}
-          />
-          <div
-            className={clsx(
-              'w-4 h-4',
-              'lg:w-6 lg:h-6 bg-greenMac rounded-full flex'
-            )}
-          />
-        </div>
-        <div id="terminal-tabs" className="flex w-11/12">
-          <div className="flex w-2/12 justify-center items-center pt-4 pb-4 text-primary-50 font-menlo border-b-2 border-solid">
-            about-me
-          </div>
-        </div>
-      </div>
+      <TerminalHeader />
       {/* terminal body */}
-      <div id="terminal-body" className="p-4 font-menlo overflow-auto">
+      <div
+        id="terminal-body"
+        className="p-4 font-menlo overflow-auto"
+        ref={body}
+      >
         <p className="mb-2 text-green">
-          <span className="animate-pulse">&gt;</span> ~/Code/andresflorezlopez:
+          <span className="animate-pulse">&gt;</span>{' '}
+          <span className="text-primary-50">~/Code/andresflorezlopez:</span>
         </p>
         <div className="text-green">
           <Typewriter
@@ -69,17 +93,26 @@ const Terminal = () => {
                 .typeString('<br/>')
                 .typeString('<br/>')
                 .pauseFor(2500)
+                .callFunction(() => {
+                  makeScrollDown();
+                })
                 .typeString(
                   `So, I'm Andres Florez. I'm an engineer that love...`
                 )
                 .typeString('<br/>')
                 .pauseFor(2500)
+                .callFunction(() => {
+                  makeScrollDown();
+                })
                 .typeString(
-                  ` beer, coding, travel and dream to become a DJ some day`
+                  ` travel, coding, enjoy the life and dream to become a DJ some day`
                 )
+                .typeString('<br/>')
+                .typeString('<br/>')
                 .pauseFor(2500)
-                .typeString('<br/>')
-                .typeString('<br/>')
+                .callFunction(() => {
+                  makeScrollDown();
+                })
                 .typeString(
                   'I have more than 10 years of experience in the tech industry'
                 )
@@ -87,12 +120,13 @@ const Terminal = () => {
                 .typeString('<br/>')
                 .pauseFor(2500)
                 .typeString('I hope you enjoy your stay in my web site')
+                .callFunction(() => {
+                  makeScrollDown();
+                })
                 .start();
             }}
           />
           <span className="text-redMac mt-4">
-            <br />
-            <br />
             <br />
             <br />
             {'(warning): Error code 400 -- site under construction'}
